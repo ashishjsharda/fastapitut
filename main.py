e0ea1b4118dc6372e1070ed84b0979a3086c4b79
Ashish Sharda <ashishjsharda@gmail.com>
2022-11-01 07:21:40 -0500

    return array

--- main.py
+++ main.py
@@ -7,3 +7,7 @@ async def root():
 @app.get("/items/{item_id}")
 async def read_item(item_id: int):
     return {"item_id": item_id}
+
+@app.get("/users")
+async def read_users():
+    return ["Ricky", "John", "Peter"]
